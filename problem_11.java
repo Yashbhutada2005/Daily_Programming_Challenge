27a5ec20c2e4ea7fa6bf74e29e0320744baf1341
Yashbhutada2005 <yashbhutada05@gmail.com>
2024-09-21 21:18:40 +0530

    .

--- problem_11.java
+++ problem_11.java
@@ -3,7 +3,6 @@ public class problem_11 {
 
     static void permuteRec(StringBuilder s, int idx) {
       
-        // Base case
         if (idx == s.length() - 1) {
             System.out.println(s);
             return;
@@ -11,23 +10,18 @@ public class problem_11 {
 
         for (int i = idx; i < s.length(); i++) {
           
-            // Swapping 
             swap(s, idx, i);
 
-            // First idx+1 characters fixed
             permuteRec(s, idx + 1);
 
-            // Backtrack
             swap(s, idx, i);
         }
     }
 
-    // Wrapper function
     static void permute(String s) {
         permuteRec(new StringBuilder(s), 0);
     }
 
-    // Helper function to swap characters in the StringBuilder
     static void swap(StringBuilder s, int i, int j) {
         char temp = s.charAt(i);
         s.setCharAt(i, s.charAt(j));
